2d8506de009c1a9898d63943b63a577776fb093f
srdjan rakic <dev_srdjanr@tenforeazure.onmicrosoft.com>
2022-03-20 22:40:43 +0100

    as

---
+++ my-app/src/features/counter/components/Counter.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+import { useAppSelector, useAppDispatch } from '../../../app/hooks';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  incrementIfOdd,
+  selectCount,
+} from '../infrastracture/redux/counterSlice';
+import styles from './Counter.module.css';
+
+export function Counter() {
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  const onClick = async () => {
+    try {
+      const originalPromiseResult = await dispatch(incrementAsync(incrementValue)).unwrap()
+      // handle result here
+    } catch (rejectedValueOrSerializedError) {
+      // handle error here
+      window.alert('alo');
+    }
+  }
+
+  return (
+    <div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+        <span className={styles.value}>{count}</span>
+        <button
+          className={styles.button}
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+      </div>
+      <div className={styles.row}>
+        <input
+          className={styles.textbox}
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button
+          className={styles.button}
+          onClick={() => dispatch(incrementByAmount(incrementValue))}
+        >
+          Add Amount
+        </button>
+        <button
+          className={styles.asyncButton}
+          onClick={() => onClick()}
+        >
+          Add Async
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => dispatch(incrementIfOdd(incrementValue))}
+        >
+          Add If Odd
+        </button>
+      </div>
+    </div>
+  );
+}
